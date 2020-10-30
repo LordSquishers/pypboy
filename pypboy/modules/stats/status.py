a128c9472c2a4ba6a092de8fce86dccbc1404f85
Andy Molloy <amolloy@gmail.com>
2020-10-30 19:54:26 +0000

    Remove the weird status text from the bottom of the screen

--- pypboy/modules/stats/status.py
+++ pypboy/modules/stats/status.py
@@ -43,7 +43,3 @@ class Health(game.Entity):
         self.image = pygame.image.load('images/pipboy.png')
         self.rect = self.image.get_rect()
         self.image = self.image.convert()
-        text = config.FONTS[18].render(
-            "Grieve - Level 27", True, (105, 251, 187), (0, 0, 0))
-        text_width = text.get_size()[0]
-        self.image.blit(text, (config.user_config['video']['width'].get() / 2 - 8 - text_width / 2, 250))
