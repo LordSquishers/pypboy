138f21c68c9c99117eb2e64f53ce77e407d8ccfd
Andy Molloy <amolloy@gmail.com>
2020-10-31 14:14:26 +0000

    Overload the 'Misc' submodule as a volume control

--- pypboy/modules/data/misc.py
+++ pypboy/modules/data/misc.py
@@ -1,6 +1,26 @@
 import pypboy
-
+from alsaaudio import Mixer
 
 class Module(pypboy.SubModule):
 
 	label = "Misc"
+
+	def __init__(self, *args, **kwargs):
+		super(Module, self).__init__(*args, **kwargs)
+
+		items = []
+		actions = []
+		for i in range(0, 8):
+			name = 'Volume ' + str(i)
+			items.append(name)
+			action = lambda i=i: self.set_volume(i)
+			actions.append(action)
+
+		self.menu = pypboy.ui.Menu(100, items, actions, 0)
+		self.menu.rect[0] = 4
+		self.menu.rect[1] = 60
+		self.add(self.menu)
+
+	def set_volume(self, i):
+		print(f"set volume {i}")
+		Mixer('Headphone').setvolume(i * 20)
